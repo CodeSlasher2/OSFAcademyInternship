03516bb74bcc5dfbb5e0d1c3eca351f44b8347e2
CodeSlasher2 <112767805+CodeSlasher2@users.noreply.github.com>
2022-09-18 22:19:55 +0300

    footer,services,featured
    footer functionality + adaptive
    services adaptive
    fefatured adaptive

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,5 +1,15 @@
-// const iconNav = document.querySelector(".icn-nav");
-
-// iconNav.addEventListener("click", () => {
-//   console.log("hover");
-// });
+//
+//FOOTER
+//
+document.addEventListener("click", (e) => {
+  const isDropdownBtn = e.target.matches("[data-dropdown-btn]");
+  if (!isDropdownBtn && e.target.closest("[data-dropdown-btn]") !== null)
+    return;
+  let currentDropdown;
+  if (isDropdownBtn) {
+    currentDropdown = e.target.closest("[data-dropdown-btn]").parentElement
+      .parentElement;
+    console.log(e.target);
+    currentDropdown.classList.toggle("active");
+  }
+});
