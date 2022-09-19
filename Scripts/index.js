9405aa176ed668cdcac89bb2d13270d3a30ba1f8
CodeSlasher2 <112767805+CodeSlasher2@users.noreply.github.com>
2022-09-19 21:23:33 +0300

    resposivel all except nav

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,4 +1,20 @@
 //
+//NAV
+//
+const navDropdown = document.querySelector(".dropdown");
+const dropdownContent = document.querySelector(".container-dropdown");
+let dropDownChilds = dropdownContent.children;
+
+const dropdownOpener = function () {
+  navDropdown.classList.toggle("active");
+};
+
+document.addEventListener("click", (e) => {
+  const isDropdownBtn = e.target.matches(".dropdown");
+  if (!isDropdownBtn && e.target.closest(".dropdown") !== null) return;
+});
+
+//
 //FOOTER
 //
 document.addEventListener("click", (e) => {
@@ -9,7 +25,19 @@ document.addEventListener("click", (e) => {
   if (isDropdownBtn) {
     currentDropdown = e.target.closest("[data-dropdown-btn]").parentElement
       .parentElement;
-    console.log(e.target);
+    // console.log(e.target);
     currentDropdown.classList.toggle("active");
   }
+});
+
+//
+//Hamburger Menu
+//
+const hamburgerMenu = document.querySelector(".menu");
+const mobileNav = document.querySelector(".nav");
+
+hamburgerMenu.addEventListener("click", function (e) {
+  e.preventDefault();
+  hamburgerMenu.classList.toggle("open");
+  mobileNav.classList.toggle("active");
 });
